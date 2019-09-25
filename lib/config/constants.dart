a63014876b844ce7dd6208208bceb119ed2803c3
lishan <pollyxiaoshan@gmail.com>
2019-09-25 18:00:55 +0800

    Replace static data with dynamic data from network for home page.

---
+++ lib/config/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+    static const String ROOT_URL = "http://jd.itying.com/";
+}
--- lib/pages/tabs/home_page.dart
+++ lib/pages/tabs/home_page.dart
@@ -1,6 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import '../../utils/screen_utils.dart' as sc_utils;
+import '../../config/constants.dart';
+import '../../model/banner_list_model.dart' as banner;
+import '../../model/hot_product_list_model.dart' as product;
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,21 +12,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<banner.Result> _bannerList = [];
+  List<product.Result> _hotProductList = [];
+  List<product.Result> _bestProductList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getBannerList();
+    _getHotProductList();
+    _getBestProductList();
+  }
+
+  void _getBannerList() async {
+    String url = "${Constants.ROOT_URL}api/focus";
+    var result = await Dio().get(url);
+    var bannerList = banner.BannerListModel.fromJson(result.data);
+    setState(() {
+      this._bannerList = bannerList.result;
+    });
+  }
+
+  void _getHotProductList() async {
+    String  url = Constants.ROOT_URL+"api/plist?is_hot=1";
+    var result = await Dio().get(url);
+    var hotProductList = product.HotProductListModel.fromJson(result.data);
+    setState(() {
+      this._hotProductList = hotProductList.result;
+    });
+  }
+
+  void _getBestProductList() async {
+    String  url = Constants.ROOT_URL+"api/plist?is_best=1";
+    var result = await Dio().get(url);
+    var bestProductList = product.HotProductListModel.fromJson(result.data);
+    setState(() {
+      this._bestProductList = bestProductList.result;
+    });
+  }
+
   Widget _generateSwiper() {
-    List<String> imgList = [
-      "https://www.itying.com/images/flutter/slide01.jpg",
-      "https://www.itying.com/images/flutter/slide02.jpg",
-      "https://www.itying.com/images/flutter/slide03.jpg",
-    ];
 
     return Container(
       child: AspectRatio(
         aspectRatio: 2 / 1,
         child: Swiper(
-          itemCount: 3,
+          itemCount: this._bannerList.length,
           itemBuilder: (BuildContext context, int index) {
             return Image.network(
-              imgList[index],
+              Constants.ROOT_URL+this._bannerList[index].pic.replaceAll("\\", "/"),
               fit: BoxFit.cover,
             );
           },
@@ -54,9 +93,7 @@ class _HomePageState extends State<HomePage> {
   Widget _likedProductList() {
     return Container(
         height: sc_utils.ScreenUtils.getHeight(240),
-        padding: EdgeInsets.only(
-            left: 10,
-            right: 10),
+        padding: EdgeInsets.only(left: 10, right: 10),
         child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemBuilder: (BuildContext context, int index) {
@@ -68,37 +105,80 @@ class _HomePageState extends State<HomePage> {
                   margin:
                       EdgeInsets.only(right: sc_utils.ScreenUtils.getWidth(20)),
                   child: Image.network(
-                    "https://www.itying.com/images/flutter/hot${index + 1}.jpg",
+                    Constants.ROOT_URL+this._hotProductList[index].sPic.replaceAll("\\", "/"),
                     fit: BoxFit.cover,
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.all(sc_utils.ScreenUtils.getWidth(5)),
-                  child: Text("第${index + 1}条"),
-                )
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.all(sc_utils.ScreenUtils.getWidth(7)),
+                  child: Text("¥"+this._hotProductList[index].price.toString(), style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 12,
+                  ),),
+                ),
               ],
             );
           },
-          itemCount: 10,
+          itemCount: this._hotProductList.length,
         ));
   }
 
-  Widget _hotRecItem() {
+  Widget _hotRecItem(product.Result product) {
     double width = (sc_utils.ScreenUtils.getScreenWidth() - 30) / 2;
     return Container(
       width: width,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black38,
-          width: 2
-        )
-      ),
+      padding: EdgeInsets.all(5),
+      decoration:
+          BoxDecoration(border: Border.all(color: Colors.black26, width: 0.5)),
       child: Column(
         children: <Widget>[
-          Image.network("https://www.itying.com/images/flutter/list1.jpg", fit: BoxFit.cover,)
+          Image.network(
+            Constants.ROOT_URL+product.pic.replaceAll("\\", "/"),
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 12
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                SizedBox(height: 12,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("¥"+ product.price.toString(), style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12
+                    ),),
+                    Text("¥"+ product.oldPrice, style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                      decoration: TextDecoration.lineThrough,
+                    ),),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  List<Widget> _hotRecItems() {
+    List<Widget> result = [];
+    for (var value in this._bestProductList) {
+      result.add(_hotRecItem(value));
+    }
+    return result;
   }
 
   @override
@@ -113,28 +193,14 @@ class _HomePageState extends State<HomePage> {
         SizedBox(height: sc_utils.ScreenUtils.getHeight(20)),
         _likedProductList(),
         _titleWidget("热门推荐"),
-
-        Padding(padding: EdgeInsets.all(10),
+        Padding(
+          padding: EdgeInsets.all(10),
           child: Wrap(
             spacing: 10,
             runSpacing: 10,
-            children: <Widget>[
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-              _hotRecItem(),
-            ],
+            children: this._hotRecItems(),
           ),
         ),
-
       ],
     );
   }
